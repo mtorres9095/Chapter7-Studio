066c4d1c2841ee5b082ca2415a0fd7b311bf4259
mtorres9095 <mtorres9095@gmail.com>
2020-06-11 11:52:20 -0400

    Add project files.

---
+++ Chapter7 Studio/Question.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter7_Studio
+{
+    class Question
+    {
+{
+        public string QuestionText { get; set; }
+        public int PointValue { get; set; }
+        
+        public Question(string questionText, int pointValue)
+        {
+            QuestionText = questionText;
+            PointValue = pointValue;
+            
+        }
+
+        public void DisplayQuestion()
+        {
+            
+        }
+
+        //public void RetestUpdate(bool size)
+        //{
+        //    Storage += size;
+        //}
+    }
+
+}
+}
